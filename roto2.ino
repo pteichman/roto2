85c6517773b5eddae6a992854f4658b9b4ce45f3
Peter Teichman <peter@teichman.org>
2015-11-14 10:53:47 -0800

    Add DrawbarOsc, a wavetable oscillator for harmonic frequencies

--- roto2.ino
+++ roto2.ino
@@ -6,55 +6,24 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+#include "drawbar_osc.h"
 #include "voice_allocator.h"
 
 // GUItool: begin automatically generated code
-AudioSynthWaveformSine   sine1;          //xy=339,52
-AudioSynthWaveformSine   sine2;          //xy=339,88
-AudioSynthWaveformSine   sine3;          //xy=339,124
-AudioSynthWaveformSine   sine4;          //xy=340,174
-AudioSynthWaveformSine   sine5;          //xy=340,209
-AudioSynthWaveformSine   sine6;          //xy=340,244
-AudioSynthWaveformSine   sine7;          //xy=340,291
-AudioSynthWaveformSine   sine8;          //xy=341,327
-AudioSynthWaveformSine   sine9;         //xy=341,412
-AudioSynthWaveformSine   sine10;         //xy=341,483
-AudioSynthWaveformSine   sine11;          //xy=342,363
-AudioSynthWaveformSine   sine12;         //xy=342,447
-AudioSynthWaveformSine   sine13;         //xy=342,447
-AudioSynthWaveformSine   sine14;         //xy=342,447
-AudioSynthWaveformSine   sine15;         //xy=342,447
-AudioSynthWaveformSine   sine16;         //xy=342,447
-AudioMixer4              mixer1;         //xy=459,325
-AudioMixer4              mixer2;         //xy=461,90
-AudioMixer4              mixer3;         //xy=461,208
-AudioMixer4              mixer4;         //xy=461,444
+DrawbarOsc               osc1;
+DrawbarOsc               osc2;
+DrawbarOsc               osc3;
+DrawbarOsc               osc4;
 AudioEffectEnvelope      envelope1;      //xy=591,90
 AudioEffectEnvelope      envelope2;      //xy=591,208
 AudioEffectEnvelope      envelope3;      //xy=593,326
 AudioEffectEnvelope      envelope4;      //xy=596,444
 AudioMixer4              mixer5;         //xy=759,274
 AudioOutputI2S           i2s1;           //xy=887,273
-AudioConnection          patchCord1(sine1, 0, mixer1, 0);
-AudioConnection          patchCord2(sine2, 0, mixer1, 1);
-AudioConnection          patchCord3(sine3, 0, mixer1, 2);
-AudioConnection          patchCord4(sine4, 0, mixer1, 3);
-AudioConnection          patchCord5(sine5, 0, mixer2, 0);
-AudioConnection          patchCord6(sine6, 0, mixer2, 1);
-AudioConnection          patchCord7(sine7, 0, mixer2, 2);
-AudioConnection          patchCord8(sine8, 0, mixer2, 3);
-AudioConnection          patchCord9(sine9, 0, mixer3, 0);
-AudioConnection          patchCord10(sine10, 0, mixer3, 1);
-AudioConnection          patchCord11(sine11, 0, mixer3, 2);
-AudioConnection          patchCord12(sine12, 0, mixer3, 3);
-AudioConnection          patchCord13(sine13, 0, mixer4, 0);
-AudioConnection          patchCord14(sine14, 0, mixer4, 1);
-AudioConnection          patchCord15(sine15, 0, mixer4, 2);
-AudioConnection          patchCord16(sine16, 0, mixer4, 3);
-AudioConnection          patchCord17(mixer1, envelope1);
-AudioConnection          patchCord18(mixer2, envelope2);
-AudioConnection          patchCord19(mixer3, envelope3);
-AudioConnection          patchCord20(mixer4, envelope4);
+AudioConnection          patchCord17(osc1, envelope1);
+AudioConnection          patchCord18(osc2, envelope2);
+AudioConnection          patchCord19(osc3, envelope3);
+AudioConnection          patchCord20(osc4, envelope4);
 AudioConnection          patchCord21(envelope1, 0, mixer5, 0);
 AudioConnection          patchCord22(envelope2, 0, mixer5, 1);
 AudioConnection          patchCord23(envelope3, 0, mixer5, 2);
@@ -79,6 +48,11 @@ void setup() {
   initVoice(3);
   initVoice(4);
 
+  mixer5.gain(0, 0.25);
+  mixer5.gain(1, 0.25);
+  mixer5.gain(2, 0.25);
+  mixer5.gain(3, 0.25);
+
   usbMIDI.setHandleNoteOff(OnNoteOff);
   usbMIDI.setHandleNoteOn(OnNoteOn);
   usbMIDI.setHandleVelocityChange(OnVelocityChange);
@@ -95,55 +69,32 @@ void loop() {
 void initVoice(byte voice) {
   switch (voice) {
     case 1:
-      initSine(&sine1);
-      initSine(&sine2);
-      initSine(&sine3);
-      initSine(&sine4);
+      osc1.Init();
+      osc1.Amplitude(1.0);
       initEnvelope(&envelope1);
-      initMixer(&mixer1);
       break;
     case 2:
-      initSine(&sine5);
-      initSine(&sine6);
-      initSine(&sine7);
-      initSine(&sine8);
+      osc2.Init();
+      osc2.Amplitude(1.0);
       initEnvelope(&envelope2);
-      initMixer(&mixer2);
       break;
     case 3:
-      initSine(&sine9);
-      initSine(&sine10);
-      initSine(&sine11);
-      initSine(&sine12);
+      osc3.Init();
+      osc3.Amplitude(1.0);
       initEnvelope(&envelope3);
-      initMixer(&mixer3);
       break;
     case 4:
-      initSine(&sine13);
-      initSine(&sine14);
-      initSine(&sine15);
-      initSine(&sine16);
+      osc4.Init();
+      osc4.Amplitude(1.0);
       initEnvelope(&envelope4);
-      initMixer(&mixer4);
       break;
   }
-}
-
-void initSine(AudioSynthWaveformSine *sine) {
-  sine->amplitude(1.0);
 }
 
 void initEnvelope(AudioEffectEnvelope *env) {
   env->attack(2.0);
   env->sustain(1.0);
   env->release(2.0);
-}
-
-void initMixer(AudioMixer4 *mix) {
-  mix->gain(0, 0.0625);
-  mix->gain(1, 0.0625);
-  mix->gain(2, 0.0625);
-  mix->gain(3, 0.0625);
 }
 
 void OnNoteOn(byte channel, byte note, byte velocity) {
@@ -160,31 +111,19 @@ void OnNoteOn(byte channel, byte note, byte velocity) {
   
   switch (voice) {
     case 0:
-      sine1.frequency(fund);
-      sine2.frequency(fund * 0.5);
-      sine3.frequency(fund * 1.5);
-      sine4.frequency(fund * 2.0);
+      osc1.Fundamental(fund);
       envelope1.noteOn();
       break;
     case 1:
-      sine5.frequency(fund);
-      sine6.frequency(fund * 0.5);
-      sine7.frequency(fund * 1.5);
-      sine8.frequency(fund * 2.0);
+      osc2.Fundamental(fund);
       envelope2.noteOn();
       break;
-    case 2:
-      sine9.frequency(fund);
-      sine10.frequency(fund * 0.5);
-      sine11.frequency(fund * 1.5);
-      sine12.frequency(fund * 2.0);
+    case 3:
+      osc3.Fundamental(fund);
       envelope3.noteOn();
       break;
-    case 3:
-      sine13.frequency(fund);
-      sine14.frequency(fund * 0.5);
-      sine15.frequency(fund * 1.5);
-      sine16.frequency(fund * 2.0);
+    case 4:
+      osc4.Fundamental(fund);
       envelope4.noteOn();
       break;
   }
@@ -264,4 +203,3 @@ float note2freq(byte note) {
   float tmp = (note-69) / 12.0;
   return 440.0 * pow(2.0, tmp);
 }
-
